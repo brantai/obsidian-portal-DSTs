d873c77cde3eb1362249314ee8686fc77f7cf380
Brent Naylor <brantai@gmail.com>
2016-03-12 15:04:25 -0800

    initial commit

---
+++ sheet.js
@@ -0,0 +1,17 @@
+/*
+ * This is the javascript specific to the minimal4e DST
+ * In this example, we're using some javascript to update the ability modifiers when someone changes their
+ * level or their ability score.
+ *
+ * The key is to use the callback functions to catch the right events.
+ * Read the top comments in characters.js to get a better idea of how the callbacks work.
+ *
+ * Copy and paste this directly into the javascript textarea on obsidianportal.com
+ */
+
+
+function fantasydice_dataPreLoad(options) {
+  // Called just before the data is loaded.
+  // alert("dataPreLoad");
+  aisleten.characters.jeditablePlaceholder = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+}
